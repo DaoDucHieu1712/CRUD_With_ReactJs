0bf5ce95817b30cab31455cb588b5a39a98b4d02
DucHieu <daoduchieu171221@gmail.com>
2023-04-10 16:14:07 +0700

    hieudd_10/04/2023_1

--- my-app/src/api/ProductApi.ts
+++ my-app/src/api/ProductApi.ts
@@ -1,36 +1,35 @@
-import { ListResponse } from "../models/Common";
-import { Product } from "../models/Product";
-import axiosClient from "./axiosClient";
+import { ListResponse, Product } from '../models/Product'
+import axiosClient from './axiosClient'
 
 const ProductApi = {
   getAll(): Promise<ListResponse<Product>> {
-    const url = "/products";
-    return axiosClient.get(url);
+    const url = '/products'
+    return axiosClient.get(url)
   },
   getProduct(id: number): Promise<Product> {
-    const url = `/products/${id}`;
-    return axiosClient.get(url);
+    const url = `/products/${id}`
+    return axiosClient.get(url)
   },
   getProductByCategory(category: string): Promise<ListResponse<Product>> {
-    const url = `/products/category/${category}`;
-    return axiosClient.get(url);
+    const url = `/products/category/${category}`
+    return axiosClient.get(url)
   },
   searchProduct(search: string): Promise<ListResponse<Product>> {
-    const url = `/products/search?q=${search}`;
-    return axiosClient.get(url);
+    const url = `/products/search?q=${search}`
+    return axiosClient.get(url)
   },
   addProduct(params: Product): Promise<Product> {
-    const url = "/products/add";
-    return axiosClient.post(url, params);
+    const url = '/products/add'
+    return axiosClient.post(url, params)
   },
   updateProduct(params: Product): Promise<Product> {
-    const url = `/products/${params.id}`;
-    return axiosClient.put(url, params);
+    const url = `/products/${params.id}`
+    return axiosClient.put(url, params)
   },
   deleteProduct(id: number): Promise<Product> {
-    const url = `/products/${id}`;
-    return axiosClient.delete(url);
-  },
-};
+    const url = `/products/${id}`
+    return axiosClient.delete(url)
+  }
+}
 
-export default ProductApi;
+export default ProductApi
